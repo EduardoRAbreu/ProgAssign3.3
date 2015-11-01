fa0b23fad9920c318e2764c241f4981d1e5ac037
EduardoRAbreu <eduardo.r.abreu@me.com>
2015-10-31 23:45:54 -0400

    final version of rankall

--- rankall-old.R
+++ rankall-old.R
@@ -4,7 +4,6 @@ rankall <- function(outcome, num = "best"){
   
   ## Check that state and outcome are valid
   valid_state <- unique(outcome_data$State)
-  valid_state <- valid_state[order(valid_state)]
   
   valid_outcome <- c("heart attack", "heart failure", "pneumonia")
   if(!(outcome %in% valid_outcome)){
@@ -24,30 +23,23 @@ rankall <- function(outcome, num = "best"){
   dataframe <- na.omit(outcome_data[,c(7, 2, condition)])
   names(dataframe) <- c("State", "hospital", "outcome")
   sorted <- dataframe[order(dataframe$State, dataframe$outcome, dataframe$hospital),]
-  hospital_results <- vector(mode = "character")
-  sought_hospital <- data.frame()
-  hospital_state <- vector(mode = "character")
   results <- data.frame()
+  sought_hospital <- data.frame()
   
   for (i in seq_along(valid_state)){
     state_subset <- subset(sorted, State == valid_state[i], drop = FALSE)
     
-    if(!is.integer(num) & num != "best")
-    num <- nrow(state_subset)
-    if(num == "best")
-      num <- 1
-    else
+    if(num == "best"){
+    num <- 1
+    }else if(num == "worst"){
+      num <- nrow(state_subset)
+    }else{
       num <- as.numeric(num)
+    }
+    sought_hospital <- state_subset[num, 2:1]
     
-    sought_hospital <- state_subset[num, 2]
-    
-    hospital_results <- c(hospital_results, sought_hospital)
-    hospital_state <- c(hospital_state, valid_state[i])
+    results <- rbind(results, sought_hospital)
   }
-  
-  results <- cbind(hospital_results, hospital_state)
-  results <- as.data.frame(results)
-  names(results) <- c("hospital", "state")
   
   return(results)
   
